ce685d6e7fbc5ed60a85618e96f40b7d5ce86665
YangQiii <95367863+YangQiii@users.noreply.github.com>
2022-03-10 21:39:28 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -65,7 +65,7 @@ function App() {
 
     // make POST request
     console.log('making POST request...');
-    fetch('<api-url>', {
+    fetch('https://1mnuvy9zk0.execute-api.us-east-2.amazonaws.com/prod/', {
       method: 'POST',
       headers: { "Content-Type": "application/json", "Accept": "text/plain" },
       body: JSON.stringify({ "image": inputFileData })
